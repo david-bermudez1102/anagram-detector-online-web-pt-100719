d135ae4e9dfb4c37024451634a13dd4cff9dd086
David Bermudez <juan.bermudez1102@outlook.com>
2019-10-18 19:23:55 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,6 +1,15 @@
 class Anagram
+
+  attr_accessor :array
   def initialize(word)
     @word = word
   end
 
+  def match(array)
+    @array = []
+    @word = @word.split()
+    array.each{ |e|
+
+    }
+  end
 end
