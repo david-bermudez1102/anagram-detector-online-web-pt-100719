ecf868651bce88ba46c491df127b83d9ed549d8b
David Bermudez <juan.bermudez1102@outlook.com>
2019-10-18 19:41:00 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,5 +1,6 @@
 class Anagram
 
+  attr_accessor :array
   def initialize(word)
     @word = word
   end
@@ -10,7 +11,7 @@ class Anagram
     array.each_with_object([]){ |e,possible_match|
       possible_match << e.split("")
       if possible_match.sort == @word.sort
-      @array << possible_match.join("")
+      @array << possible_match.join
       @array
     end
     }
